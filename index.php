16f9da13cd39ab23f305cb2465195ef547a6abe7
donmashe <83496777+donmashe@users.noreply.github.com>
2021-06-05 06:13:16 -0300

    add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+<h1><p style="text-align:center">WELCOME TO THIS PAGE</h1></P>  
+<link rel="stylesheet" type="text/css" href="css/style3.css">
+<a href="login.php"><b>Login to Account</b><br/>
+<a href="register.php"><b>Register</b></>
+<body>
+</body>
+</html>
